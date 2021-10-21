884b10d4cf744c222ca69e360d9559ebd3e7c84d
Sarah <sarahlchavez@gmail.com>
2021-10-21 10:48:04 -0700

    commit tree 86584841f3963c2d403fbc7e331256900ae397ae

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Your code here
+// for each class, create the method speak
+
+class Cat {
+    //...for an instance of Cat, speak returns "name says meow!"
+    constructor(name) {
+        this.name = name;
+    }
+    speak() {
+        return `${this.name} says meow!`;
+    }
+  }
+  
+  class Dog {
+    //...for an instance of Dog, speak returns "name says woof!"
+    constructor(name) {
+        this.name = name;
+    }
+    speak() {
+        return `${this.name} says woof!`;
+    }
+  }
+  
+  class Bird {
+    //...
+    constructor(name, gender) {
+        this.name = name;
+        this.gender = gender;
+    }
+    speak() {
+        if(this.gender === "male"){
+            return `It's me! ${this.name}, the parrot!`;
+    
+        } else return `${this.name} says squawk!`;
+    }
+  }
